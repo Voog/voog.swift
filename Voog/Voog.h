25435951c3a1c54b4b8ece8133951c75e371523f
Priit Haamer <priit@fraktal.ee>
2015-07-12 13:37:15 +0300

    Initial Commit

---
+++ Voog/Voog.h
@@ -0,0 +1,19 @@
+//
+//  Voog.h
+//  Voog
+//
+//  Created by Priit Haamer on 12.07.15.
+//  Copyright © 2015 Voog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Voog.
+FOUNDATION_EXPORT double VoogVersionNumber;
+
+//! Project version string for Voog.
+FOUNDATION_EXPORT const unsigned char VoogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Voog/PublicHeader.h>
+
+
